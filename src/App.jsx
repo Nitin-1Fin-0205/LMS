a610d7837742f176db913022e31318a79cde0ad3
Nitin Gupta <nitingupta1906@gmail.com>
2025-05-22 16:58:54 +0530

    Nitin :: token fixed

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const AppContent = () => {
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
-    const token = params.get('token');
+    const token = params.get('access_token');
 
     if (token) {
       localStorage.setItem('authToken', token);
